6527927197fe256d6703c1c89a39364d130816d9
zakuni <kunio038@gmail.com>
2013-03-17 22:27:00 +0900

    scroll to definite place, when scrolled.

--- jquery.smoothsnap.js
+++ jquery.smoothsnap.js
@@ -1,4 +1,7 @@
 (function( $ ){
   $.fn.smoothsnap = function() {
+    $(window).scroll(function(){
+        $("body").animate({scrollTop: 200}, 1000);
+    });
   };
 }( jQuery ));
--- sample.js
+++ sample.js
@@ -1,2 +1,3 @@
 $(function(){
+  $("h2").smoothsnap();
 });
