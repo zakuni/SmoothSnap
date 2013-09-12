887a250c0e8fbab7d53a6cfbd4de48f79e283625
zakuni <kunio038@gmail.com>
2013-09-12 20:58:01 +0900

    prevent wheel scroll

--- jquery.smoothsnap.js
+++ jquery.smoothsnap.js
@@ -1,7 +1,16 @@
 (function( $ ){
   $.fn.smoothsnap = function() {
-    $(window).scroll(function(){
-      $("html,body").animate({scrollTop: 200}, 1000);
-    });
+//    $(window).scroll(function(event){
+//        event.preventDefault();
+//        return false;
+//      $("html,body").animate({scrollTop: 200}, 1000);
+//    });
+
+    window.onmousewheel = document.onmousewheel = function(e) {
+      e = e || window.event;
+        if (e.preventDefault)
+            e.preventDefault();
+        e.returnValue = false;
+    };
   };
 }( jQuery ));
