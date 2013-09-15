4a8728439750d0a514dac22cd155459ed0476fc0
zakuni <kunio038@gmail.com>
2013-09-15 15:03:34 +0900

    change scrollAmount and setStep to private method

--- smoothsnap.coffee
+++ smoothsnap.coffee
@@ -4,20 +4,20 @@ $.widget('ui.smoothsnap',
 
     @element.on("slidestart", (event, ui) =>
       startPosition = ui.value
-      setStep(@element, 1)
+      this._setStep(@element, 1)
     )
     @element.on("slide", (event, ui) =>
-      if (scrollAmount(startPosition, ui.value) <= 10)
-        setStep(@element, 1)
-      else if (scrollAmount(startPosition, ui.value) <= 50)
-        setStep(@element, 10)
+      if (this._scrollAmount(startPosition, ui.value) <= 10)
+        this._setStep(@element, 1)
+      else if (this._scrollAmount(startPosition, ui.value) <= 50)
+        this._setStep(@element, 10)
       else
-        setStep(@element, 50)
+        this._setStep(@element, 50)
     )
 
-    scrollAmount = (start, current) ->
-      Math.abs(current - start)
+  _scrollAmount: (start, current) ->
+    Math.abs(current - start)
 
-    setStep = (element, val) ->
-      element.slider("option", "step", val)
+  _setStep: (element, val) ->
+    element.slider("option", "step", val)
 )
--- smoothsnap.js
+++ smoothsnap.js
@@ -2,28 +2,28 @@
 (function() {
   $.widget('ui.smoothsnap', {
     _create: function() {
-      var scrollAmount, setStep, startPosition,
+      var startPosition,
         _this = this;
       startPosition = 0;
       this.element.on("slidestart", function(event, ui) {
         startPosition = ui.value;
-        return setStep(_this.element, 1);
+        return _this._setStep(_this.element, 1);
       });
-      this.element.on("slide", function(event, ui) {
-        if (scrollAmount(startPosition, ui.value) <= 10) {
-          return setStep(_this.element, 1);
-        } else if (scrollAmount(startPosition, ui.value) <= 50) {
-          return setStep(_this.element, 10);
+      return this.element.on("slide", function(event, ui) {
+        if (_this._scrollAmount(startPosition, ui.value) <= 10) {
+          return _this._setStep(_this.element, 1);
+        } else if (_this._scrollAmount(startPosition, ui.value) <= 50) {
+          return _this._setStep(_this.element, 10);
         } else {
-          return setStep(_this.element, 50);
+          return _this._setStep(_this.element, 50);
         }
       });
-      scrollAmount = function(start, current) {
-        return Math.abs(current - start);
-      };
-      return setStep = function(element, val) {
-        return element.slider("option", "step", val);
-      };
+    },
+    _scrollAmount: function(start, current) {
+      return Math.abs(current - start);
+    },
+    _setStep: function(element, val) {
+      return element.slider("option", "step", val);
     }
   });
 
